37996ef9d680c00573e57f3f6660094fe0af5fa8
Jody Lipkin <lipkinjody@gmail.com>
2017-09-18 20:59:17 -0700

    look, i learned binary!

---
+++ binary.js
@@ -0,0 +1,20 @@
+//given a number in base 10, print in base 10 the max number of consecutive 1s
+
+
+function binaryOneCounter(n){
+  let maxOnes = 0;
+  while (n > 0) {
+    let remainder = n%2;
+    if (remainder === 1){
+      maxOnes++
+    } else if (remainder === 0) {
+      maxOnes = 0
+    }
+    n = (n-remainder)/2
+  }
+  console.log(maxOnes)
+  
+}
+
+binaryOneCounter(5);
+binaryOneCounter(7);
