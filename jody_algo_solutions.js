8d5712916f333b83383a45712b98d9e811894069
Jody Lipkin <lipkinjody@gmail.com>
2018-01-03 18:28:53 -0800

    added algocast exercises

--- jody_algo_solutions.js
+++ jody_algo_solutions.js
@@ -107,7 +107,6 @@
 //  }
 //}
 //
-//module.exports = fizzBuzz;
 
 
 // --- Directions
@@ -182,11 +181,26 @@
 //      return true;
 //    }
 //  }
-//  
-//  
 //}
 //
-//module.exports = anagrams;
+
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+//
+//function capitalize(str) {
+//  let array = str.split(' ');
+//  let newArray = []
+//  for (let word of array) {
+//    newArray.push(word[0].toUpperCase() + word.slice(1));
+//  }
+//  return newArray.join(' ');
+//}
 
 
 
