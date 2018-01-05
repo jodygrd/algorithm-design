11b9e513c5c4d2fc968118f105429cd574e0b95c
Jody Lipkin <lipkinjody@gmail.com>
2018-01-04 22:42:40 -0800

    steps

--- jody_algo_solutions.js
+++ jody_algo_solutions.js
@@ -202,7 +202,69 @@
 //  return newArray.join(' ');
 //}
 
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
 
+
+
+//Jody's Non-Recursive Solution. Double For loop == O(n^2) == BAD
+//function steps(n) {
+//
+//  for (i = 0; i < n; i++) {
+//    let string = ''
+//    
+//    for (j = 0; j < n; j++ ) {
+//      if (j <= i) {
+//        string += '#'
+//      } else {
+//        string += ' '
+//      }
+//    }
+//    
+//    console.log(string)
+//  }
+//  
+//}
+
+//Stephen's Recursive Solution
+
+//function steps(n, row = 0, stair = '') {
+//  if (row === n) {
+//    return;
+//  }
+//    
+//  if (n === stair.length) {
+//    console.log(stair);
+//    steps(n, row + 1);
+//    return
+//  }
+//  
+//  if (stair.length <= row) {
+//    stair += '#';
+//  } else {
+//    stair += ' '
+//  }
+//  
+//  steps(n, row, stair);
+//}
+//
+//module.exports = steps;
 
 
 
