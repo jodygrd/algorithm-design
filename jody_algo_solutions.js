7b23869d149855c48086348023869f4db324a556
Jody Lipkin <lipkinjody@gmail.com>
2018-01-05 19:06:23 -0800

    pyramid

--- jody_algo_solutions.js
+++ jody_algo_solutions.js
@@ -1,3 +1,4 @@
+
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
@@ -266,6 +267,44 @@
 //
 //module.exports = steps;
 
+
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+//iterative solution
+//function pyramid(n) {
+//  const width = n * 2 - 1;
+//  const midpoint = Math.floor(width / 2)
+//  
+//  for (let row = 0; row < n; row++){
+//    let level = ''
+//    for ( let col = 0; col < width;  col++){
+//      if (midpoint - row <= col && midpoint + row >= col) {
+//        level += '#';
+//      } else {
+//        level += ' ';
+//      }      
+//    }
+//    
+//    console.log(level)
+//  }
+//
+//}
+//
+//module.exports = pyramid;
 
 
 
