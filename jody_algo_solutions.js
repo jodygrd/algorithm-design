b6ded19e7905c21f3393b6036738dc2d35b939b8
Jody Lipkin <lipkinjody@gmail.com>
2018-01-09 18:06:50 -0800

    fib memoized

--- jody_algo_solutions.js
+++ jody_algo_solutions.js
@@ -435,3 +435,38 @@ function matrix(n) {
 //module.exports = fib;
 //
 
+//Recursive, memoized solution - including generic memoize function
+
+//bullshit fucking recursive sol'n
+function memoize(fn){
+  const cache = {};
+  
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+    
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    
+    return result;
+  }
+  
+}
+
+
+function fib(n) {
+  
+  if (n < 2) {
+    return n;
+  }
+  
+  return fib(n-1) + fib(n-2);
+  
+}
+
+fib = memoize(fib);
+
+module.exports = fib;
+
+
