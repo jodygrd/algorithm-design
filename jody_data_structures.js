3b45d974494e45e217c072b6a35d4ad3e27fb623
Jody Lipkin <lipkinjody@gmail.com>
2018-01-26 20:45:52 -0800

    node tree

--- jody_data_structures.js
+++ jody_data_structures.js
@@ -418,3 +418,44 @@ function fromLast(list, n) {
   
   
 }
+
+// --- Directions
+// 1) Create a node class.  The constructor
+// should accept an argument that gets assigned
+// to the data property and initialize an
+// empty array for storing children. The node
+// class should have methods 'add' and 'remove'.
+// 2) Create a tree class. The tree constructor
+// should initialize a 'root' property to null.
+// 3) Implement 'traverseBF' and 'traverseDF'
+// on the tree class.  Each method should accept a
+// function that gets called with each element in the tree
+
+class Node {
+  
+  constructor(data) {
+    this.data = data;
+    this.children = [];
+  }
+  
+  add(data) {
+    this.children.push(new Node(data));
+  }
+  
+//  remove(data) {
+//    const i = this.children.findIndex((child) => child.data === data);
+//    this.children.splice(i,1);
+//  }
+//  
+  remove(data) {
+    this.children = this.children.filter(child => child.data !== data);
+  }
+}
+
+class Tree {
+  constructor {
+    this.root = null;
+  }
+  
+  
+}
