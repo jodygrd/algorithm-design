5c503a71132c99a14cd8f376b3abcc40284a0874
Jody Lipkin <lipkinjody@gmail.com>
2018-01-29 13:35:28 -0800

    width of tree

--- jody_data_structures.js
+++ jody_data_structures.js
@@ -492,3 +492,39 @@ class Tree {
   
   
 }
+
+
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+function levelWidth(root) {
+  
+  let counters = [0]
+  let queue = [root, 'STOP']
+
+  while (queue.length > 1) {
+    if (queue[0] === 'STOP') {
+      counters.push(0);
+      queue.push(queue.shift());
+    }
+    let node = queue.shift();
+    queue.push(...node.children)
+    counters[counters.length-1]++
+    
+  }
+  
+  return counters;
+  
+}
+
+module.exports = levelWidth;
