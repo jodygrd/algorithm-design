7ce2d05fec554f0f5ba052b902ea58247096ffe9
Jody Lipkin <lipkinjody@gmail.com>
2018-02-03 22:48:56 -0800

    max product of 3 in an array

--- InterviewCake/IC_003.js
+++ InterviewCake/IC_003.js
@@ -7,28 +7,51 @@
 
 
 function highestProductOfThree(array){
+  
+    let highestInt = Math.max(array[0], array[1])
+    let lowestInt = Math.min(array[0], array[1])
 
 
-	let min = array[0]
-	let second = array[1]
-	let third = array[2]
-	const maxProduct = first*second*third;
+	let highestProductofTwo = array[0]*array[1];
+	let lowestProductofTwo = array[0]*array[1];
+    
+    let maxProduct = array[0]*array[1]*array[2];
+    
+    
+	for (let i = 2; i < array.length; i++) {
 
-	if (array.length === 3) {
-		return maxProduct
-	}
-
-	for (let i = 3; i < array.length; i++) {
-		if (array[i] > (first || second || third)) {
-			
-		}
-
-		let maxProduct = first*second*third;
+                           
+        if ((array[i] * highestProductofTwo || array[i] * lowestProductofTwo) > maxProduct) {
+          maxProduct = Math.max(array[i]*highestProductofTwo, array[i]*lowestProductofTwo);
+        }
+        if ((array[i] * highestInt || array[i] * lowestInt) > highestProductofTwo) {
+          highestProductofTwo = Math.max(array[i]*highestInt, array[i]*lowestInt);
+        }
+        if ((array[i] * highestInt || array[i] * lowestInt) < lowestProductofTwo) {
+          lowestProductofTwo = Math.min(array[i]*highestInt, array[i]*lowestInt);
+        }
+        if (array[i] > highestInt){
+          highestInt = array[i];
+        }
+        if (array[i] < lowestInt) {
+          lowestInt = array[i]
+        }
+      
+      //Interview Cake Suggested solution - much cleaner!
+//      maxProduct = Math.max(maxProduct, array[i]*highestProductofTwo, array[i]*lowestProductofTwo);
+//     
+//      highestProductofTwo = Math.max(highestProductofTwo, highestInt*array[i], lowestInt*array[i]);
+//      
+//      lowestProductofTwo = Math.min(highestProductofTwo, highestInt*array[i], lowestInt*array[i]);
+//      
+//      highestInt = Math.max(highestInt, array[i]);
+//      
+//      lowestInt = Math.min(lowestInt, array[i]);
 	}
 	return maxProduct;
 
 }
 
-console.log(highestProductOfThree([1,2,0,4,-1]))
+console.log(highestProductOfThree([0,0,0,-1,-1,-4,2]))
 
-//expect 8
+//expect 120
