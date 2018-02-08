8104d3b945b4a393af203f4fc9fb2f1a9fb09897
Jody Lipkin <lipkinjody@gmail.com>
2018-02-08 09:07:56 -0800

    trie

---
+++ InterviewCake/IC_011.js
@@ -0,0 +1,6 @@
+//how to save memory on "visited" webpages
+//Use a trie. What. The Hell. Is. A. Trie. 
+
+//a trie is a way of storing (in a nested object) lots of related, ordered pieces of data
+
+//my brain is so not equipped to learn this today, so we're just gonna give it a try another day. 
